923296a8b17a73aadcf019c6d8da6f7eac7ecc7f
julioc98 <julioc.santos98@gmail.com>
2018-04-19 17:17:26 -0300

    Heroes and HTTP

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -4,7 +4,7 @@ export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     const heroes = [
       { id: 11, name: 'Mr. Nice' },
-      { id: 12, name: 'Narco' },
+      { id: 12, name: 'QUITO' },
       { id: 13, name: 'Bombasto' },
       { id: 14, name: 'Celeritas' },
       { id: 15, name: 'Magneta' },
